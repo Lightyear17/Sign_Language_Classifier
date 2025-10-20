e2913b885cf4a76c5f3e3fed1685738f1cd3e4b4
Lightyear17 <shubham.patel9624@gmail.com>
2025-10-21 02:30:53 +0530

    fix: Added Cors

--- Backend/src/app.py
+++ Backend/src/app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 
 from src import logger
@@ -10,7 +11,13 @@ app = FastAPI()
 
 app.add_middleware(ServiceMiddleware)
 
-
+app.add_middleware(
+       CORSMiddleware,
+       allow_origins=["*"],
+       allow_credentials=True,
+       allow_methods=["*"],
+       allow_headers=["*"],
+   )
 @app.get("/")
 async def read_root():
     logger.info("Root endpoint accessed")
