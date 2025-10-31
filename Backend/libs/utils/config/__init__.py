7289ec8940e629489e68a2ad9e0010f9dd0521f2
Lightyear17 <shubham.patel9624@gmail.com>
2025-10-31 18:51:58 +0530

    chore: updated the config file.

--- Backend/libs/utils/config/__init__.py
+++ Backend/libs/utils/config/__init__.py
@@ -4,12 +4,22 @@ from dotenv import dotenv_values
 env_path = ".env"
 
 if not path.exists(env_path):
-    raise Exception(".env request_file not found")
+    raise Exception(".env file not found")
 
 config = dotenv_values(env_path)
 
+HOST = config.get("HOST", "127.0.0.1")
 
-HOST = config.get("HOST")
 _port_raw = config.get("PORT")
 PORT = int(_port_raw) if _port_raw else 8000
-MODEL_PATH = config.get("MODEL_PATH")
+
+MODEL_PATHS = {
+    "STATIC_MODEL": config.get("STATIC_MODEL_PATH"),
+    "REALTIME_MODEL": config.get("REALTIME_MODEL_PATH"),
+}
+
+MODEL_LABELS = {
+    "STATIC_LABELS": config.get("STATIC_LABELS_PATH"),
+    "REALTIME_LABELS": config.get("REALTIME_LABELS_PATH"),
+}
+
